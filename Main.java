6ae78f24fe52e6c188e2b76c8f91320305f67d8f
Danil <62771824+Panteli280@users.noreply.github.com>
2020-03-27 23:51:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+            Start list = new Start();
+            list.addAll("sfdsf", "fsDSdfss", "asdefsdaS", "sssssss", "rewreSdadS");
+            System.out.println(list);
+            list.updateLinkedByTask('S');
+            System.out.println(list);
+    }
+}
