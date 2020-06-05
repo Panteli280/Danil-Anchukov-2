36d27c2706385db0c3bbc9a945d20e7e05ce831f
Danil <62771824+Panteli280@users.noreply.github.com>
2020-06-06 01:10:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
 package com.company;
 
-import java.util.LinkedList;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(1);
+        LinkedList list = new LinkedList();
         list.add(2);
+        list.add(0);
         list.add(28);
-        list.add(23);
+        list.add(1);
         list.task(list);
-        int t = list.size();
-        System.out.println(t);
+        int[] array = list.toArray();
     }
 }
